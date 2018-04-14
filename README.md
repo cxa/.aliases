f1768efc7b2ebe68c871f638da14e9470fb6c677
CHEN Xian-an <xianan.chen@gmail.com>
2018-04-15 02:36:26 +0800

    Add mad_new_app

---
+++ README.md
@@ -0,0 +1,5 @@
+# .aliases
+
+Clone to `~` and add `$HOME/.aliases` to `$PATH`.
+
+- `mad_new_app.sh app_name`: Create a N2O app, rename all `sample` filenames and Erlang atoms.
---
+++ mad_new_app.sh
@@ -0,0 +1,9 @@
+mad app $1
+cd $1/apps
+mv sample $1
+cd $1/src
+mv sample.app.src $1.app.src
+mv sample.erl $1.erl
+cd ../../..
+find . -type f -exec sed -i '' 's/sample/test/g' {} \;
+echo ":name \"$1\"\n:node-sname \"$1\"\n:lib-dirs '(\"deps\")" > .edts
