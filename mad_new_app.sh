43fcb5678306db4730f692651b3fd112ac5c0e9c
CHEN Xian-an <xianan.chen@gmail.com>
2018-04-15 15:05:43 +0800

    Fix sed replace

--- mad_new_app.sh
+++ mad_new_app.sh
@@ -5,5 +5,6 @@ cd $1/src
 mv sample.app.src $1.app.src
 mv sample.erl $1.erl
 cd ../../..
-find . -type f -exec sed -i '' 's/sample/test/g' {} \;
+find . -type f -exec sed -i '' "s/sample/$1/g" {} \;
 echo ":name \"$1\"\n:node-sname \"$1\"\n:lib-dirs '(\"deps\")" > .edts
+
