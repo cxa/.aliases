ac111ddf37371ecb9924f3f9cf8a9909c274bb19
CHEN Xian-an <xianan.chen@gmail.com>
2018-04-24 02:26:37 +0800

    Morden n2o and deps

--- mad_new_app_with_edts.sh
+++ mad_new_app_with_edts.sh
@@ -5,5 +5,14 @@ cd $1/src
 mv sample.app.src $1.app.src
 mv sample.erl $1.erl
 cd ../../..
+wget https://raw.githubusercontent.com/synrc/n2o/master/samples/rebar.config -O rebar.config
+sed -i '' '/n2o,/d' rebar.config
+sed -i '' $'/deps,[ ]*\[/s/\[/\[\\\n\    {n2o, \"\.\*\", \{git, \"git\:\/\/github\.com\/cxa\/n2o\", \{tag, \"master\"\}\}\},\\\n/g' rebar.config
+sed -i '' '/gproc,/s/tag,[ ]*\".*\"/tag, \"0.7.0"/g' rebar.config
+sed -i '' '/jsone,/s/tag,[ ]*\".*\"/tag, \"1.4.5"/g' rebar.config
+sed -i '' '/mad,/s/tag,[ ]*\".*\"/tag, \"3.9"/g' rebar.config
+sed -i '' '/sh,/s/tag,[ ]*\".*\"/tag, \"1.9.1"/g' rebar.config
+sed -i '' '/active,/s/tag,[ ]*\".*\"/tag, \"3.3"/g' rebar.config
+sed -i '' '/fs,/s/tag,[ ]*\".*\"/tag, \"4.1"/g' rebar.config
 find . -type f -exec sed -i '' "s/sample/$1/g" {} \;
 echo ":name \"$1\"\n:node-sname \"$1\"\n:lib-dirs '(\"deps\")" > .edts
