f5f91ce90e8c590c7bf33a685517af1bde71383e
Realazy Xian-an CHEN <xianan.chen@gmail.com>
2018-05-05 02:56:34 +0800

    Make mad_new_app_with_edts work with Linux

--- mad_new_app_with_edts.sh
+++ mad_new_app_with_edts.sh
@@ -1,3 +1,9 @@
+if [ `uname` == "Darwin" ]; then
+    I='-i ""'
+else
+    I='-i""'
+fi
+
 mad app $1
 cd $1/apps
 mv sample $1
@@ -6,5 +12,5 @@ mv sample.app.src $1.app.src
 mv sample.erl $1.erl
 cd ../../..
 cp ~/.aliases/rebar.config ./
-find . -type f -exec sed -i '' "s/sample/$1/g" {} \;
+find . -type f -exec sed $I "s/sample/$1/g" {} \;
 echo ":name \"$1\"\n:node-sname \"$1\"\n:lib-dirs '(\"deps\")" > .edts
