68bd3e08a0a79add150871f096b6f200d0417a9d
Realazy Xian-an CHEN <xianan.chen@gmail.com>
2018-05-08 02:16:26 +0800

    Fix sed under linux

--- mad_new_app_with_edts.sh
+++ mad_new_app_with_edts.sh
@@ -1,9 +1,3 @@
-if [ `uname` == "Darwin" ]; then
-    I='-i ""'
-else
-    I='-i""'
-fi
-
 mad app $1
 cd $1/apps
 mv sample $1
@@ -12,5 +6,6 @@ mv sample.app.src $1.app.src
 mv sample.erl $1.erl
 cd ../../..
 cp ~/.aliases/rebar.config ./
-find . -type f -exec sed $I "s/sample/$1/g" {} \;
+find . -type f -exec sed -i.bak "s/sample/$1/g" {} \;
+find . -name "*.bak" -type f -delete
 echo ":name \"$1\"\n:node-sname \"$1\"\n:lib-dirs '(\"deps\")" > .edts
